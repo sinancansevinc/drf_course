d273ef0f632d2f10fbcccd2a0503582ff5d43cf9
sinancansevinc <sinancan_sevinc@hotmail.com>
2023-02-07 19:51:39 +0300

    app is loaded

--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path,include
 from django.contrib import admin
-from django.urls import path
+from rest_framework import routers
 
-urlpatterns = [
+router = routers.DefaultRouter()
+
+urlpatterns = router.urls
+
+urlpatterns += [
     path('admin/', admin.site.urls),
+
 ]
